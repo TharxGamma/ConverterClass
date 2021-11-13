8e2dbfe9e403252b2e6eca2b33bbb1e2bdb4ffba
Tharx <tharxs_gamma@hotmail.com>
2021-11-13 21:14:57 +0000

    Fixed IntToHex function. Only binary to go, can't overload constructor again

--- Practicing.cpp
+++ Practicing.cpp
@@ -7,20 +7,21 @@ public:
 
     Converter();
     Converter(char Hex);
+    Converter(int Integer);
 
 
     int HexToInteger(char Hex);
     std::string IntegerToHex(int Integer);
     std::string IntegerToBinary(int Input);
 
-    int GetHexValue() { return Hex; }
+    std::string GetHexValue() { return Hex; }
     int GetIntegerValue() { return Integer; }
     std::string GetBinaryValue() { return Binary; }
 
 private:
 
     int Integer = {0};
-    int Hex = {0};
+    std::string Hex = {0};
     std::string Binary = {0};
 
 };
@@ -30,222 +31,222 @@ Converter::Converter()
 
 }
 
+Converter::Converter(int InputInteger)
+{
+    this->Hex = IntegerToHex(InputInteger);
+} 
+
 Converter::Converter(char InputHex)
 {
-    this->Integer = HexToInteger(InputHex); 
-} 
+    this->Integer = HexToInteger(InputHex);
+}
 
 
 int Converter::HexToInteger(char Hex)
 {
-   switch (Hex)
-   {
-    case '0':
+    switch (Hex)
     {
-        int Zero = 0;
-        return Zero;
+        case '0':
+        {
+            int Zero = 0;
+            return Zero;
+        }
+
+
+        case '1':
+        {
+            int One = 1;
+            return One;
+        }
+
+        case '2':
+        {
+            int Two = 2;
+            return Two;
+        }
+
+        case '3':
+        {
+            int Three = 3;
+            return Three;
+        }
+
+        case '4':
+        {
+            int Four = 4;
+            return Four;
+        }
+
+        case '5':
+        {
+            int Five = 5;
+            return Five;
+        }
+
+        case '6':
+        {
+            int Six = 6;
+            return Six;
+        }
+
+        case '7':
+        {
+            int Seven = 7;
+            return Seven;
+        }
+
+        case '8':
+        {
+            int Eight = 8;
+            return Eight;
+        }
+
+        case '9':
+        {
+            int Nine = 9;
+            return Nine;
+        }
+
+        case 'a':
+        {
+            int Ten = 10;
+            return Ten;
+        }
+
+        case 'b':
+        {
+            int Eleven = 11;
+            return Eleven;
+        }
+
+        case 'c':
+        {
+            int Twelve = 12;
+            return Twelve;
+        }
+
+        case 'd':
+        {
+            int Thirteen = 13;
+            return Thirteen;
+        }
+
+        case 'e':
+        {
+            int Fourteen = 14;
+            return Fourteen;
+        }
+
+        case 'f':
+        {
+            int Fithteen = 15;
+            return Fithteen;
+        }
+
     }
-
-
-    case '1':
-    {
-        int One = 1;
-        return One;
-    }
-
-    case '2':
-    {
-        int Two = 2;
-        return Two;
-    }
-
-    case '3':
-    {
-        int Three = 3;
-        return Three;
-    }
-
-    case '4':
-    {
-        int Four = 4;
-        return Four;
-    }
-
-    case '5':
-    {
-        int Five = 5;
-        return Five;
-    }
-
-    case '6':
-    {
-        int Six = 6;
-        return Six;
-    }
-
-    case '7':
-    {
-        int Seven = 7;
-        return Seven;
-    }
-
-    case '8':
-    {
-        int Eight = 8;
-        return Eight;
-    }
-
-    case '9':
-    {
-        int Nine = 9;
-        return Nine;
-    }
-
-    case 'a':
-    {
-        int Ten = 10;
-        return Ten;
-    }
-
-    case 'b':
-    {
-        int Eleven = 11;
-        return Eleven;
-    }
-
-    case 'c':
-    {
-        int Twelve = 12;
-        return Twelve;
-    }
-
-    case 'd':
-    {
-        int Thirteen = 13;
-        return Thirteen;
-    }
-
-    case 'e':
-    {
-        int Fourteen = 14;
-        return Fourteen;
-    }
-
-    case 'f':
-    {
-        int Fithteen = 15;
-        return Fithteen;
-    }
-
-    default:
-        std::cout << "No suitable conversions" << std::endl;
-   }
-
 }
 
 std::string Converter::IntegerToHex(int Integer)
 {
     switch (Integer)
     {
-    case 0:
-    {
-        std::string Zero = "0";
-        return Zero;
-    }
+        case 0:
+        {
+            std::string Zero = "0";
+            return Zero;
+        }
 
-    case 1:
-    {
-        std::string One = "1";
-        return One;
-    }
+        case 1:
+        {
+            std::string One = "1";
+            return One;
+        }
 
-    case 2:
-    {
-        std::string Two = "2";
-        return Two;
-    }
+        case 2:
+        {
+            std::string Two = "2";
+            return Two;
+        }
 
-    case 3:
-    {
-        std::string Three = "3";
-        return Three;
-    }
+        case 3:
+        {
+            std::string Three = "3";
+            return Three;
+        }
 
-    case 4:
-    {
-        std::string Four = "4";
-        return Four;
-    }
+        case 4:
+        {
+            std::string Four = "4";
+            return Four;
+        }
 
-    case 5:
-    {
-        std::string Five = "5";
-        return Five;
-    }
+        case 5:
+        {
+            std::string Five = "5";
+            return Five;
+        }
 
-    case 6:
-    {
-        std::string Six = "6";
-        return Six;
-    }
+        case 6:
+        {
+            std::string Six = "6";
+            return Six;
+        }
 
-    case 7:
-    {
-        std::string Seven = "7";
-        return Seven;
-    }
+        case 7:
+        {
+            std::string Seven = "7";
+            return Seven;
+        }
 
-    case 8:
-    {
-        std::string Eight = "8";
-        return Eight;
-    }
+        case 8:
+        {
+            std::string Eight = "8";
+            return Eight;
+        }
 
-    case 9:
-    {
-        std::string Nine = "9";
-        return Nine;
-    }
+        case 9:
+        {
+            std::string Nine = "9";
+            return Nine;
+        }
 
-    case 10:
-    {
-        std::string A = "A";
-        return A;
-    }
+        case 10:
+        {
+            std::string A = "A";
+            return A;
+        }
 
-    case 11:
-    {
-        std::string B = "B";
-        return B;
-    }
+        case 11:
+        {
+            std::string B = "B";
+            return B;
+        }
 
-    case 12:
-    {
-        std::string C = "C";
-        return C;
-    }
+        case 12:
+        {
+            std::string C = "C";
+            return C;
+        }
 
-    case 13:
-    {
-        std::string D = "D";
-        return D;
-    }
+        case 13:
+        {
+            std::string D = "D";
+            return D;
+        }
 
-    case 14:
-    {
-        std::string E = "E";
-        return E;
-    }
+        case 14:
+        {
+            std::string E = "E";
+            return E;
+        }
 
-    case 15:
-    {
-        std::string F = "F";
-        return F;
-    }
-    default:
-        std::cout << "No suitable conversions" << std::endl;
-    }
+        case 15:
+        {
+            std::string F = "F";
+            return F;
+        }
 
+    }
 }
 
 std::string Converter::IntegerToBinary(int Input)
@@ -274,8 +275,10 @@ std::string Converter::IntegerToBinary(int Input)
 int main()
 {
     Converter ConvertHexToInteger('a');
+    Converter ConvertIntegerToHex(10);
 
     std::cout << ConvertHexToInteger.GetIntegerValue() << std::endl;
+    std::cout << ConvertIntegerToHex.GetHexValue() << std::endl;
 
     
 }
